988b02de18abd276081cfd3eb0cac4eea74e2666
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-12-02 21:46:49 -0800

    forms.py is finally presentable enough to push

---
+++ forms.py
@@ -0,0 +1,20 @@
+from wtforms import StringField
+from flask_wtf import FlaskForm
+from wtforms.fields.simple import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+
+class UserAddForm(FlaskForm):
+    """"Form for adding new user"""
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    
+class LoginForm(FlaskForm):
+    """Login form"""
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    
+    
+    
