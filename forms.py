92c06dc60a80585ef34fac040bfd3ee10bae5bac
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-12-03 20:05:22 -0800

    Actively working on this section

--- forms.py
+++ forms.py
@@ -1,13 +1,14 @@
-from wtforms import StringField
 from flask_wtf import FlaskForm
+from wtforms import StringField
 from wtforms.fields.simple import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
+from models import User
 
 class UserAddForm(FlaskForm):
     """"Form for adding new user"""
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
     
