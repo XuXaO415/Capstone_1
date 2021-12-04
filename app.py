024a90f70ed16eb68bd4e22d3e44ad36df28c8d2
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-12-03 21:54:33 -0800

    Headers are impacting API calls

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from sqlalchemy.exc import IntegrityError
 from forms import UserAddForm, LoginForm
 from models import db, connect_db, User
 #################################################################################
-# from secrets import API_KEY, BASE_URL
-# from dotenv import load_dotenv
-# load_dotenv()
+# from secrets import API_KEY
+from dotenv import load_dotenv
+load_dotenv()
 #################################################################################
 # https: // jonathansoma.com/lede/foundations-2019/classes/apis/keeping-api-keys-secret/
 
@@ -21,67 +21,70 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('API_KEY', os.getenv('API_KEY'))
+# app.config['SECRET_KEY'] = os.environ.get('API_KEY', os.getenv('API_KEY'))
 # app.config['SECRET_KEY'] = 'API_KEY'
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
+app.config['API_KEY'] = os.getenv("API_KEY")
+app.config['host'] = os.getenv("host")
+
 
 connect_db(app)
 
-# BASE_URL = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
-# # url = f"https://api-hoaxy.p.rapidapi.com/latest-articles"
 
 ##############################################################################
-#User signup/login/logout
+# User signup/login/logout
+
 
 @app.before_request
 def add_user_to_g():
     """When logged in, adds curr_user to Flask global"""
-    
+
     if CURR_USER_KEY in session:
         g.user = User.query.get(session[CURR_USER_KEY])
-        
+
     else:
         g.user = None
-        
-        
+
+
 def do_login(user):
     """Logs in user"""
-    
-    session[CURR_USER_KEY] = user.id 
-    
+
+    session[CURR_USER_KEY] = user.id
+
+
 def do_logout():
     """Logs out user"""
     if CURR_USER_KEY in session:
         del session[CURR_USER_KEY]
-        
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     """Handles user signup; stores it in db"""
-    
+
     if CURR_USER_KEY in session:
         del session[CURR_USER_KEY]
 
     form = UserAddForm()
-    
+
     if form.validate_on_submit():
         try:
-            user = User.signup( 
-                first_name = form.first_name.data,
-                last_name = form.last_name.data,
-                email = form.email.data,
-                username = form.username.data,
-                password = form.password.data
+            user = User.signup(
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                email=form.email.data,
+                username=form.username.data,
+                password=form.password.data
             )
             db.session.commit()
-            
+
         except IntegrityError:
             flash("Sorry, that username is taken.", "danger")
             return render_template("users/signup.html", form=form)
-        
+
         do_login(user)
         return redirect("/")
-    
+
     else:
         return render_template("users/signup.html", form=form)
 
@@ -89,22 +92,22 @@ def signup():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Handle user login"""
-    
+
     form = LoginForm()
-    
+
     if form.validate_on_submit():
         user = User.authenticate(form.username.data,
                                  form.password.data)
-        
+
         if user:
             do_login(user)
             flash(f"Welcome back, {user.username}!", "success")
             return redirect("/")
-        
+
         flash("Invalid credentials.", "danger")
-        
+
     return render_template("users/login.html", form=form)
-    
+
 
 @app.route("/logout")
 def logout():
@@ -113,10 +116,11 @@ def logout():
     do_logout()
     flash("You have successfully logged out of Aletheia!", "success")
     return redirect("/login")
-        
+
 ##############################################################################
 # Homepage and error pages
-        
+
+
 @app.route("/")
 def homepage():
     """Show homepage"""
@@ -128,16 +132,29 @@ def homepage():
 
 ##############################################################################
 
+
 @app.route("/list-articles")
 def list_articles():
     """List all articles"""
+
+
     url = "https://api-hoaxy.p.rapidapi.com/top-articles"
-    
-    
-    
-        
+
+    querystring = {"most_recent": "true"}
+
+    headers = {
+    'x-rapidapi-host': host,
+    'x-rapidapi-key': API_KEY
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
 ##############################################################################
-#Turn off all caching in Flask
+# Turn off all caching in Flask
+
+
 @app.after_request
 def add_header(req):
     """Add non-caching headers on every request."""
